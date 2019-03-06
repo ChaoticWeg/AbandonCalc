f3da7de29fd440b325832380edcb36a8ac990e1b
ChaoticWeg <shawn@chaoticweg.cc>
2019-03-06 00:19:29 -0600

    re-introduce paddingBottom

--- styles/IconTextRow.js
+++ styles/IconTextRow.js
@@ -3,12 +3,16 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
     iconText: {
         flexDirection: 'row',
-        alignItems: 'center'
+        alignItems: 'center',
+        paddingBottom: 10
     },
     label: {
         color: '#fff',
         fontFamily: 'Helvetica',
         fontSize: 20,
         marginLeft: 10
+    },
+    icon: {
+        width: '150%'
     }
 });
